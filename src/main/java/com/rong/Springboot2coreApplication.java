8c4d7ca240e44c0a3fcfc8e279d62c19e9cee12d
aihuxi <worongbo@163.com>
2018-10-02 16:37:18 +0800

    初始化 spring2.0核心

---
+++ src/main/java/com/rong/Springboot2coreApplication.java
@@ -0,0 +1,12 @@
+package com.rong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2coreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2coreApplication.class, args);
+	}
+}
